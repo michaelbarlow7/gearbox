a411927d6e75b9087c305ecb56bd34c7287af063
Michael Barlow <michael.barlow@vivant.com.au>
2014-07-26 18:04:51 +1000

    Populating info windows

--- app/src/main/java/com/gearboxer/gearbox/MapActivity.java
+++ app/src/main/java/com/gearboxer/gearbox/MapActivity.java
@@ -1,13 +1,16 @@
 package com.gearboxer.gearbox;
 
 import android.app.Activity;
-import android.location.Location;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.gearboxer.gearbox.model.Gear;
 import com.gearboxer.gearbox.model.GearLocation;
+import com.gearboxer.gearbox.model.GearType;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -23,11 +26,14 @@ public class MapActivity extends Activity {
     private GoogleMap mMap;
     private LatLng myLocation;
     private List<GearLocation> locationList;
+    private GearLocationOnInfoWindowClickListener gearLocationOnInfoWindowClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        gearLocationOnInfoWindowClickListener = new GearLocationOnInfoWindowClickListener();
 
         setUpMapIfNeeded();
 
@@ -47,11 +53,47 @@ public class MapActivity extends Activity {
 
                 LayoutInflater inflater = LayoutInflater.from(MapActivity.this);
                 View view = inflater.inflate(R.layout.marker_info, null);
-                TextView text = (TextView) view.findViewById(R.id.markerText);
+                TextView text = (TextView) view.findViewById(R.id.markerInfoTitle);
                 text.setText(gearLocation.getName());
+
+                List<Gear> gears = gearLocation.getGearList();
+                for (Gear gear : gears){
+                    if (gear.getGearType() == GearType.VOLLEYBALL){
+                        View icon = view.findViewById(R.id.volleyball);
+                        icon.setVisibility(View.VISIBLE);
+                        continue;
+                    }
+                    if (gear.getGearType() == GearType.TENNIS){
+                        View icon = view.findViewById(R.id.tennis);
+                        icon.setVisibility(View.VISIBLE);
+                        continue;
+                    }
+                    if (gear.getGearType() == GearType.SOCCER){
+                        View icon = view.findViewById(R.id.soccer);
+                        icon.setVisibility(View.VISIBLE);
+                        continue;
+                    }
+                    if (gear.getGearType() == GearType.BASKETBALL){
+                        View icon = view.findViewById(R.id.basketball);
+                        icon.setVisibility(View.VISIBLE);
+                        continue;
+                    }
+                    if (gear.getGearType() == GearType.BEACHBALL){
+                        View icon = view.findViewById(R.id.beachball);
+                        icon.setVisibility(View.VISIBLE);
+                        continue;
+                    }
+                    if (gear.getGearType() == GearType.CRICKET){
+                        View icon = view.findViewById(R.id.cricket);
+                        icon.setVisibility(View.VISIBLE);
+                        continue;
+                    }
+                }
+
                 return view;
             }
         });
+        mMap.setOnInfoWindowClickListener(gearLocationOnInfoWindowClickListener);
 //        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
 //
 //            @Override
@@ -96,6 +138,18 @@ public class MapActivity extends Activity {
                 // The Map is verified. It is now safe to manipulate the map.
 
             }
+        }
+    }
+
+    private class GearLocationOnInfoWindowClickListener implements GoogleMap.OnInfoWindowClickListener {
+
+        public GearLocationOnInfoWindowClickListener(){
+        }
+        @Override
+        public void onInfoWindowClick(Marker marker) {
+            int gearLocationPosition = Integer.parseInt(marker.getSnippet());
+            GearLocation gearLocation = locationList.get(gearLocationPosition);
+            Toast.makeText(MapActivity.this, "GO TO : " + gearLocation.getName(), Toast.LENGTH_LONG).show();
         }
     }
 }
